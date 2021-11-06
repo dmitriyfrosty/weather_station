c07bc17f42e17859381882cee6a628b81b568ccc
dmitriyfrosty <diman.morozov16@yandex.ru>
2021-11-06 14:47:34 +0700

    1.0

--- Transmitter/transmitter/transmitter.ino
+++ Transmitter/transmitter/transmitter.ino
@@ -1,3 +1,10 @@
+/*   Данный скетч делает следующее: передатчик (TX) отправляет массив
+     данных, который генерируется согласно показаниям с кнопки и с
+     двух потенциомтеров. Приёмник (RX) получает массив, и записывает
+     данные на реле, сервомашинку и генерирует ШИМ сигнал на транзистор.
+    by AlexGyver 2016
+*/
+
 #include <SPI.h>          // библиотека для работы с шиной SPI
 #include "nRF24L01.h"     // библиотека радиомодуля
 #include "RF24.h"         // ещё библиотека радиомодуля
@@ -10,12 +17,10 @@ byte address[][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"}; //в
 byte counter;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
+  Serial.begin(9600);         // открываем порт для связи с ПК
 
   radio.begin();              // активировать модуль
-  radio.setAutoAck(0);        // режим подтверждения приёма, 1 вкл 0 выкл
+  radio.setAutoAck(1);        // режим подтверждения приёма, 1 вкл 0 выкл
   radio.setRetries(0, 15);    // (время между попыткой достучаться, число попыток)
   radio.enableAckPayload();   // разрешить отсылку данных в ответ на входящий сигнал
   radio.setPayloadSize(32);   // размер пакета, в байтах
@@ -23,7 +28,7 @@ void setup() {
   radio.openWritingPipe(address[0]);  // мы - труба 0, открываем канал для передачи данных
   radio.setChannel(0x60);             // выбираем канал (в котором нет шумов!)
 
-  radio.setPALevel (RF24_PA_MAX);   // уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
+  radio.setPALevel (RF24_PA_MIN);   // уровень мощности передатчика. На выбор RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH, RF24_PA_MAX
   radio.setDataRate (RF24_250KBPS); // скорость обмена. На выбор RF24_2MBPS, RF24_1MBPS, RF24_250KBPS
   //должна быть одинакова на приёмнике и передатчике!
   //при самой низкой скорости имеем самую высокую чувствительность и дальность!!
@@ -33,9 +38,9 @@ void setup() {
 }
 
 void loop() {
-  counter = analogRead(4);
   Serial.print("Sent: ");
   Serial.println(counter);
   radio.write(&counter, sizeof(counter));
-  delay(1000);
+  counter++;
+  delay(500);
 }
